10a1117ef4714444c4c6aa56223be98bfdaf3a36
fazcholoth <falilkamali@gmail.com>
2023-04-26 20:46:44 +0530

    envupdated

--- utils/twilio.js
+++ utils/twilio.js
@@ -1,6 +1,6 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Set environment variables for your credentials
-// Read more at http://twil.io/secure
+
+require('dotenv').config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const serviceSid = process.env.TWILIO_SERVICE_SID;
